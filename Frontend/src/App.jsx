b9c0ad757ed3b021b6a0be3a2e91c08befe9bca7
Gouri Agarwal <gouriagarwal15@gmail.com>
2024-04-04 17:59:53 +0530

    landing page

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Banner from './Components/Banner'
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Banner/>
+    </>
+  )
+}
+
+export default App
